fb8d4307869d435bdd16f427bce9ae814bbd830b
Alma Babic <alma.babic@tacta.io>
2025-01-07 16:47:04 +0100

    Header and Sidebar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent],    // This is an array where you list all the components, directives, and pipes that belong to this module.
-  imports: [BrowserModule],        // This is an array where you import other Angular modules that your module depends on. BrowserModule provides the necessary functionalities to run the app in the browser.
-  bootstrap: [AppComponent]        // This property specifies which component should be bootstrapped/loaded when the application starts. 
+  declarations: [AppComponent, HeaderComponent, SidebarComponent], // This is an array where you list all the components, directives, and pipes that belong to this module.
+  imports: [BrowserModule], // This is an array where you import other Angular modules that your module depends on. BrowserModule provides the necessary functionalities to run the app in the browser.
+  bootstrap: [AppComponent], // This property specifies which component should be bootstrapped/loaded when the application starts.
 })
 export class AppModule {}
