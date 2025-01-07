5dce8a084a2e1c28aad4ab94b364a8b9ce120317
Alma Babic <alma.babic@tacta.io>
2025-01-07 14:28:29 +0100

    Project structure

---
+++ frontend/src/app/shopper/shopper.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopper',
+  imports: [],
+  templateUrl: './shopper.component.html',
+  styleUrl: './shopper.component.css'
+})
+export class ShopperComponent {
+
+}
