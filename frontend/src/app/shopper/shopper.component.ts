51906d5208acf8647b036de88be9acd5a02b4645
Alma Babic <alma.babic@tacta.io>
2025-01-10 13:37:51 +0100

    Fixed shopping component

--- frontend/src/app/shopper/shopper.component.ts
+++ frontend/src/app/shopper/shopper.component.ts
@@ -9,19 +9,25 @@ import { ShopperService } from './services/shopper.service';
   standalone: false,
 })
 export class ShopperComponent implements OnInit {
-  shoppers: Shopper[] = [];  // This array will store the list of shoppers fetched from the API.
+  shoppers: Shopper[] = []; // This array will store the list of shoppers fetched from the API.
 
-  constructor(private shopperService: ShopperService) {}  // The shopperService is injected into the component 
+  constructor(private shopperService: ShopperService) {} // The shopperService is injected into the component
 
   ngOnInit() {
-    this.shopperService.getShoppers().subscribe( // The component subscribes to this observable to receive the shopper data. The subscription will trigger an HTTP request to fetch the list of shoppers from the API.
-      (data) => {
-        this.shoppers = data;   // Storing fetched shoppers; The data (which is an array of shoppers) is assigned to the shoppers property of the component.
-        console.log(data);
-      },
-      (error) => {
-        console.error('Error fetching shoppers', error);
-      }
+    this.getShoppers();  // calling the method below
+  }
+
+  getShoppers() {
+    this.shopperService.getShoppers().subscribe(  // The component subscribes to this observable to receive the shopper data. The subscription will trigger an HTTP request to fetch the list of shoppers from the API.
+      {
+        next: (data) => {
+          this.shoppers = data; // Storing fetched shoppers; The data (which is an array of shoppers) is assigned to the shoppers property of the component.
+          console.log(data);
+        },
+        error: (error) => {
+          console.error('Error fetching shoppers', error);
+        },
+      } 
     );
   }
 }
