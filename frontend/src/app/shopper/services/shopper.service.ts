47381cfcaf4274ab1f1c4858e0e8ef6b580e9754
Alma Babic <alma.babic@tacta.io>
2025-01-14 10:49:43 +0100

    Changed shoppers url in shopper service

--- frontend/src/app/shopper/services/shopper.service.ts
+++ frontend/src/app/shopper/services/shopper.service.ts
@@ -12,7 +12,7 @@ export class ShopperService {
   constructor(private http: HttpClient) { } // This is the constructor of the ShopperService class. It injects an instance of the HttpClient into the service.
 
   getShoppers(): Observable<Shopper[]> {  // Fetching the list of shoppers. It returns an Observable<Shopper[]>, meaning it returns an observable that, when subscribed to, will emit an array of Shopper objects.
-    return this.http.get<Shopper[]>(this.apiUrl); 
+    return this.http.get<Shopper[]>(this.apiUrl + '/shoppers'); 
     // The http.get method is used to fetch data from the API. 
     // The generic <Shopper[]> ensures that the response is typed as an array of Shopper objects. 
     // The apiUrl is the URL for the backend service to get the shopper data. 
