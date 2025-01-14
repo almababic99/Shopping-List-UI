69dc38a359d5f728e80dcddd7c123e50ccbde742
Alma Babic <alma.babic@tacta.io>
2025-01-14 16:01:07 +0100

    Item and Shopper service url fix

--- frontend/src/app/item/services/item.service.ts
+++ frontend/src/app/item/services/item.service.ts
@@ -12,7 +12,7 @@ export class ItemService {
   constructor(private http: HttpClient) { } // This is the constructor of the ItemService class. It injects an instance of the HttpClient into the service.
 
   getItems(): Observable<Item[]> {  // Fetching the list of items. It returns an Observable<Item[]>, meaning it returns an observable that, when subscribed to, will emit an array of Item objects.
-    return this.http.get<Item[]>(this.apiUrl + '/items'); 
+    return this.http.get<Item[]>(`${this.apiUrl}/items`); 
     // The http.get method is used to fetch data from the API. 
     // The generic <Item[]> ensures that the response is typed as an array of Item objects. 
     // The apiUrl is the URL for the backend service to get the item data. 
@@ -23,14 +23,14 @@ export class ItemService {
   }
 
   addItem(item: Item)  {   // Adding item on backend
-    return this.http.post(this.apiUrl + '/addItem', item);  // Sending http post request to backend with 'item' as parameter; addItem is returning Observable
+    return this.http.post(`${this.apiUrl}/addItem`, item);  // Sending http post request to backend with 'item' as parameter; addItem is returning Observable
   }
 
   deleteItem(id: number) {  // Deleting item on backend
-    return this.http.delete(this.apiUrl + `/deleteItem/${id}`);   // Sending http delete request to backend with 'id' as parameter: deleteItem is returning Observable
+    return this.http.delete(`${this.apiUrl}/deleteItem/${id}`);   // Sending http delete request to backend with 'id' as parameter: deleteItem is returning Observable
   }
 
   editItem(item: Item) {  // Editing item on backend
-    return this.http.put(this.apiUrl + `/editItem/${item.id}`, item)    // Sending http put request to backend with 'item' as parameter: editItem is returning Observable
+    return this.http.put(`${this.apiUrl}/editItem/${item.id}`, item)    // Sending http put request to backend with 'item' as parameter: editItem is returning Observable
   }
 }
